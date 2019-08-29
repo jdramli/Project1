8b7a87f5710b3363415b697c916c30ab86f057e1
Joseph Ramli <jramli1@binghamton.edu>
2019-08-29 14:58:48 -0400

    update fixed case mismatch?

--- app/src/main/java/com/example/project1_ctof/MainActivity.java
+++ app/src/main/java/com/example/project1_ctof/MainActivity.java
@@ -1,17 +1,16 @@
 package com.example.project1_ctof;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TextView;
+import com.example.project1_ctof.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -26,17 +25,42 @@ public class MainActivity extends AppCompatActivity {
         //handles from content_main buttons/views
         final TextView display_temperature = findViewById(R.id.temperature);
         final TextView user_num = findViewById(R.id.user_num);
+
         FloatingActionButton fab = findViewById(R.id.fab);
+        FloatingActionButton fab2 = findViewById(R.id.fab2);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //temperature = 107; //This works, need to set it to the value of user input now instead of a constant
-                temperature = Double.valueOf(user_num.getText().toString()); // This crashes the app if no number is input -- use try/catch block
-                temperature = temperature * 9/5 + 32;
-                //temperature.setText(Double.toString(temperature));
-                display_temperature.setText(Double.toString(temperature));
-                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                //        .setAction("Action", null).show();
+                try{
+                    temperature = Double.valueOf(user_num.getText().toString()); // This crashes the app if no number is input -- use try/catch block
+                    temperature = temperature * 9/5 + 32;
+                    //temperature.setText(Double.toString(temperature));
+                    display_temperature.setText(Double.toString(temperature));
+                    //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    //        .setAction("Action", null).show();
+                }
+                catch(Exception e){
+                    display_temperature.setText("Error, enter only numbers for the temperature");
+                }
+
+            }
+        });
+        fab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //temperature = 107; //This works, need to set it to the value of user input now instead of a constant
+                try{
+                    temperature = Double.valueOf(user_num.getText().toString()); // This crashes the app if no number is input -- use try/catch block
+                    temperature = (temperature-32) * 5/9;
+                    //temperature.setText(Double.toString(temperature));
+                    display_temperature.setText(Double.toString(temperature));
+                    //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    //        .setAction("Action", null).show();
+                }
+                catch(Exception e){
+                    display_temperature.setText("Error, enter only numbers for the temperature");
+                }
             }
         });
     }
